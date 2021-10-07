1180c79be056662209a5adc2b0e3d0650588a570
Krunal Jumde <krunaljumde24@gmail.com>
2021-10-07 08:09:28 +0530

    First commit spring boot for project setup

---
+++ src/test/java/com/employeeMgmt/Employee/Mangement/EmployeeMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeMgmt.Employee.Mangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
